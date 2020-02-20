26df6e499e6fefbfac94efe8eb30f609b9c65cd1
BozhinovUSMC <61009695+BozhinovUSMC@users.noreply.github.com>
2020-02-20 17:31:08 +0200

    Homework1

---
+++ Задача 2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] Arrays = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Arrays[i] = Console.ReadLine();
+            }
+
+            for (int i = n-1; i >= 0; i--)
+            {
+                Console.WriteLine(Arrays[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
