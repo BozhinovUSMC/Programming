29aacb3aab5a420c092efb218f9cdfeb1c66cbec
BozhinovUSMC <61009695+BozhinovUSMC@users.noreply.github.com>
2020-03-20 13:19:34 +0200

    .gitignore

---
+++ Homework 25March/Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_25March
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = Console.ReadLine();
+
+                int length = myString.Length;
+
+                Console.WriteLine($"wihout first 3: {myString.Substring(3)}");
+                Console.WriteLine($"without last 3: {myString.Substring(0, length - 3)}");
+                Console.WriteLine($"without first and last 3: {myString.Substring(3, length - 6)}");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
