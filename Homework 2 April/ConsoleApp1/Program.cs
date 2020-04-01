c79797b69dedea205a0014eec0aaa00d6cb30f77
BozhinovUSMC <61009695+BozhinovUSMC@users.noreply.github.com>
2020-04-01 10:51:23 +0300

    homework

---
+++ Homework 2 April/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[rows, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.WriteLine(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int m = 1000;
+                for (int row = 0; row < rows; row++)
+                {
+                    if (matrix[row, col] < m) m = matrix[row, col];
+                }
+                Console.WriteLine("{0} ", m);
+            }
+            Console.ReadLine();
+        }
+    }
+}
