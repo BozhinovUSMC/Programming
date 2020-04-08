1748e0278c8e5bd93289ac8ab6e18a991a94919d
BozhinovUSMC <61009695+BozhinovUSMC@users.noreply.github.com>
2020-04-08 20:41:12 +0300

    Homework

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new Dictionary<string, string>();
+
+            var input = Console.ReadLine().Split(' ');
+            string numbers = input[0].ToLower();
+
+            switch (numbers)
+            {
+                case "A":
+                    {
+                        phonebook.Add(input[1], input[2]);
+                        break;
+                    }
+
+                case "S":
+                    {
+                        if (phonebook.ContainsKey(input[1]))
+                        {
+                            Console.WriteLine($"Name: {input[1]} --> {phonebook[input[1]]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Contact doesn't exist");
+                        }
+                        break;                    
+                    }
+
+                case "stop":
+                   {
+                        return;
+                   }
+                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dictionary/Ex 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var materials = new Dictionary<string, int>();                               
+            materials.Add("gold", 0);
+            materials.Add("silver", 0);
+            materials.Add("copper", 0);
+
+            while (true)
+            {
+                Console.WriteLine("Enter type of material :");
+                string comm = Console.ReadLine().ToLower();                
+                if (comm == "gold" || comm == "silver" || comm == "copper" || comm == "stop");
+                {
+                    
+                    if (comm != "stop")
+                    {
+                        Console.WriteLine($"Enter amount{comm}:");
+                        int amount = int.Parse(Console.ReadLine());
+                        switch (comm)
+                        {
+
+                            case "Gold":
+                                {
+                                    materials["Gold"] += amount;
+                                    break;
+                                }
+
+                            case "Silver":
+                                {
+                                    materials["Silver"] += amount;
+                                    break;
+                                }
+
+                            case "Copper":
+                                {
+                                    materials["Copper"] += amount;
+                                    break;
+                                }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Gold --> {materials["Gold"]}");
+                        Console.WriteLine($"Silver --> {materials["Silver"]}");
+                        Console.WriteLine($"Copper --> {materials["Copper"]}");
+                        break;
+                    }
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
